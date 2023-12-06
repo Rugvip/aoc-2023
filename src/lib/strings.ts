6e78339018dfd8599d19e5bbd4ab419d6066676b
Patrik Oldsberg <poldsberg@gmail.com>
2023-12-06 12:11:09 +0100

    lib add initial string utilities

---
+++ src/lib/strings.ts
@@ -0,0 +1,14 @@
+export type TrimRight<
+  S extends string,
+  TTrim extends string = ' '
+> = S extends `${infer IRest}${TTrim}` ? TrimLeft<IRest, TTrim> : S;
+
+export type TrimLeft<
+  S extends string,
+  TTrim extends string = ' '
+> = S extends `${TTrim}${infer IRest}` ? TrimLeft<IRest, TTrim> : S;
+
+export type Trim<S extends string, TTrim extends string = ' '> = TrimLeft<
+  TrimRight<S, TTrim>,
+  TTrim
+>;
