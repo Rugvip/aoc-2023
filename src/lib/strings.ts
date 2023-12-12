c4f7a68a8c7810878505bed2031b50b4e1c23192
Patrik Oldsberg <poldsberg@gmail.com>
2023-12-12 19:49:48 +0100

    lib/strings: add Join

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -1,3 +1,6 @@
+import { array } from './array';
+import { test } from './test';
+
 export namespace strings {
   export type TrimRight<
     S extends string,
@@ -20,4 +23,41 @@ export namespace strings {
   > = S extends `${infer IChar}${infer IRest}` ? ToChars<IRest, [...TArr, IChar]> : TArr;
 
   export type ToNumber<S extends string> = S extends `${infer N extends number}` ? N : never;
+
+  export type Join<
+    TArr extends (string | number)[],
+    TJoin extends string = '',
+    TResult extends string = '',
+  > = TArr extends [
+    infer IA extends string | number,
+    infer IB extends string | number,
+    ...infer IRest extends (string | number)[],
+  ]
+    ? Join<[IB, ...IRest], TJoin, TResult extends '' ? `${IA}` : `${TResult}${TJoin}${IA}`>
+    : TArr extends [infer IA extends string | number]
+    ? TResult extends ''
+      ? `${IA}`
+      : `${TResult}${TJoin}${IA}`
+    : '';
+
+  declare const testJoin: test.Describe<
+    test.Expect<Join<[]>, ''>,
+    test.Expect<Join<[], '-'>, ''>,
+    test.Expect<Join<[1], ''>, '1'>,
+    test.Expect<Join<[1], '-'>, '1'>,
+    test.Expect<Join<[1, 2], ''>, '12'>,
+    test.Expect<Join<[1, 2], '-'>, '1-2'>,
+    test.Expect<Join<[1, 2, 3], ''>, '123'>,
+    test.Expect<Join<[1, 2, 3], '-'>, '1-2-3'>,
+    test.Expect<Join<[1, 2, 3], `${',' | '-'}`>, `1${',' | '-'}2${',' | '-'}3`>,
+    test.Expect<Join<['a'], ''>, 'a'>,
+    test.Expect<Join<['a'], '-'>, 'a'>,
+    test.Expect<Join<['a', 'b'], ''>, 'ab'>,
+    test.Expect<Join<['a', 'b'], '-'>, 'a-b'>,
+    test.Expect<Join<['a', 'b', 'c'], ''>, 'abc'>,
+    test.Expect<Join<['a', 'b', 'c'], '-'>, 'a-b-c'>,
+    test.Expect<Join<['a', 'b', 'c'], `${',' | '-'}`>, `a${',' | '-'}b${',' | '-'}c`>,
+    test.Expect<Join<array.Make<20, '1'>, ''>, '11111111111111111111'>,
+    test.Expect<Join<array.Make<20, '1'>, '0'>, '101010101010101010101010101010101010101'>
+  >;
 }
