e574b6c5531d04b0ca0a858bbf524defc2a34bb8
Patrik Oldsberg <poldsberg@gmail.com>
2023-12-30 14:42:13 +0100

    lib/bigint: update utils to use strings

--- src/lib/bigint/utils.ts
+++ src/lib/bigint/utils.ts
@@ -1,8 +1,7 @@
 import { test } from '../test';
-import { AddIntegers } from './Add';
-import { Integer, ToInteger, FromInteger } from './types';
+import { Add } from './Add';
 
-export type Negate<T extends number | string> = T extends 0
+export type Negate<T extends string | number> = T extends 0
   ? 0
   : `${T}` extends `-${infer N}`
   ? N
@@ -10,40 +9,36 @@ export type Negate<T extends number | string> = T extends 0
   ? N
   : never;
 
-export type IsNegative<T extends number> = `${T}` extends `-${string}` ? true : false;
+export type IsNegative<T extends string | number> = `${T}` extends `-${string}` ? true : false;
 
 export type SignOf<T extends string | number> = `${T}` extends `-${string}` ? '-' : '+';
 
-export type Abs<T extends number> = `${T}` extends `-${infer N extends number}` ? N : T;
+export type Abs<T extends string | number> = `${T}` extends `-${infer N}` ? N : `${T}`;
 
 declare const testAbs: test.Describe<
-  test.Expect<Abs<0>, 0>,
-  test.Expect<Abs<-1>, 1>,
-  test.Expect<Abs<1>, 1>,
-  test.Expect<Abs<-99>, 99>,
-  test.Expect<Abs<99>, 99>
+  test.Expect<Abs<0>, '0'>,
+  test.Expect<Abs<-1>, '1'>,
+  test.Expect<Abs<1>, '1'>,
+  test.Expect<Abs<-99>, '99'>,
+  test.Expect<Abs<99>, '99'>
 >;
 
-export type Inc<TA extends number | string> = ToInteger<TA> extends infer IA extends Integer
-  ? FromInteger<AddIntegers<IA, Integer<'+', [1]>>>
-  : never;
+export type Inc<TA extends string | number> = Add<TA, '1'>;
 
 declare const testInc: test.Describe<
-  test.Expect<Inc<0>, 1>,
-  test.Expect<Inc<-1>, 0>,
-  test.Expect<Inc<1>, 2>,
-  test.Expect<Inc<999999>, 1000000>
+  test.Expect<Inc<0>, '1'>,
+  test.Expect<Inc<-1>, '0'>,
+  test.Expect<Inc<1>, '2'>,
+  test.Expect<Inc<999999>, '1000000'>
 >;
 
-export type Dec<TA extends number | string> = ToInteger<TA> extends infer IA extends Integer
-  ? FromInteger<AddIntegers<IA, Integer<'-', [1]>>>
-  : never;
+export type Dec<TA extends string | number> = Add<TA, '-1'>;
 
 declare const testDec: test.Describe<
-  test.Expect<Dec<0>, -1>,
-  test.Expect<Dec<-1>, -2>,
-  test.Expect<Dec<1>, 0>,
-  test.Expect<Dec<1000000>, 999999>
+  test.Expect<Dec<0>, '-1'>,
+  test.Expect<Dec<-1>, '-2'>,
+  test.Expect<Dec<1>, '0'>,
+  test.Expect<Dec<1000000>, '999999'>
 >;
 
 export type TrimZeroes<S extends string> = S extends '0'
