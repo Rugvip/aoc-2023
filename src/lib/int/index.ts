1d1d33bbc25fefa0647f9d08ec942b4421e7d869
Patrik Oldsberg <poldsberg@gmail.com>
2023-12-30 13:51:15 +0100

    lib/int: split out utils

--- src/lib/int/index.ts
+++ src/lib/int/index.ts
@@ -1,52 +1,7 @@
-import { test } from '../test';
-import { AddIntegers } from './Add';
-import { Integer, ToInteger, FromInteger } from './types';
-
 export type { Compare, Min, Max } from './Compare';
 export type { Divide } from './Divide';
 export type { Multiply } from './Multiply';
 export type { Subtract } from './Subtract';
 export type { Add } from './Add';
 export type { Bit, Digit, Sign, Integer, TrimLeading0, ToInteger, FromInteger } from './types';
-
-export type FlipSign<T extends number> = T extends 0
-  ? 0
-  : `${T}` extends `-${infer N extends number}`
-  ? N
-  : `-${T}` extends `${infer N extends number}`
-  ? N
-  : never;
-
-export type IsNegative<T extends number> = `${T}` extends `-${string}` ? true : false;
-
-export type Abs<T extends number> = `${T}` extends `-${infer N extends number}` ? N : T;
-
-declare const testAbs: test.Describe<
-  test.Expect<Abs<0>, 0>,
-  test.Expect<Abs<-1>, 1>,
-  test.Expect<Abs<1>, 1>,
-  test.Expect<Abs<-99>, 99>,
-  test.Expect<Abs<99>, 99>
->;
-
-export type Inc<TA extends number | string> = ToInteger<TA> extends infer IA extends Integer
-  ? FromInteger<AddIntegers<IA, Integer<'+', [1]>>>
-  : never;
-
-declare const testInc: test.Describe<
-  test.Expect<Inc<0>, 1>,
-  test.Expect<Inc<-1>, 0>,
-  test.Expect<Inc<1>, 2>,
-  test.Expect<Inc<999999>, 1000000>
->;
-
-export type Dec<TA extends number | string> = ToInteger<TA> extends infer IA extends Integer
-  ? FromInteger<AddIntegers<IA, Integer<'-', [1]>>>
-  : never;
-
-declare const testDec: test.Describe<
-  test.Expect<Dec<0>, -1>,
-  test.Expect<Dec<-1>, -2>,
-  test.Expect<Dec<1>, 0>,
-  test.Expect<Dec<1000000>, 999999>
->;
+export type { FlipSign, IsNegative, Abs, Inc, Dec } from './utils';
